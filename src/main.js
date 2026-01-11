80c6af4e8f8677bab7bbcef9324d9e5c64cd4dcb
yogeswar142 <amarayogeswararao16@gmail.com>
2026-01-11 16:50:21 +0530

    Final Version

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ document.getElementById("app").innerHTML = `
 <div class="max-w-6xl mx-auto p-4">
   <div class="bg-white p-4 rounded-xl shadow-xl">
     <h1 class="text-2xl md:text-3xl font-bold text-center text-indigo-700 mb-4">
-      GITAM SGPA Calculator
+      SGPA Calculator
     </h1>
 
     <div class="hidden md:block overflow-x-auto">
